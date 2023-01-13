0445fe8bada2c1c5b4e0de83157e7a6e305c0231
Ahmed-Sameh-Mostafa <ahmeds.elshahid@gmail.com>
2023-01-13 14:47:22 +0200

    solved 01_01_01

---
+++ unit_02_C_Programming/Assignments/01_01_01.c
@@ -0,0 +1,16 @@
+/*
+ * 	File Name:01_01_01.c
+ *
+ *  Created on: 13 Jan 2023
+ *  Author: Ahmed Sameh
+ *  Problem Statment: Write C Program to Print a Sentence
+ */
+
+#include <stdio.h>
+
+int main (void)
+{
+	printf("Road to Mastering Embedded Systems");
+	return 0;
+}
+
